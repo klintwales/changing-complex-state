8fe1a672723b1f4a6cf8b74474d1eb5820b66fdc
Klint <klintwales@gmail.com>
2021-03-20 19:01:57 -0500

    allowed fName and lName to change state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  var [fullName, setFullName] = useState({
+    fName:"",
+    lName:""
+  });
+
+  function changeName(event){
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullName(prevValue => {
+      if (inputName === "fName"){
+        return {
+          fName: newValue,
+          lNmae: prevValue.lName
+        }
+      }else if(inputName === "lName"){
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        }
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+        onChange={changeName}
+        name="fName"
+        placeholder="First Name"
+        value={fullName.fName}
+        />
+        <input
+        onChange={changeName}
+        name="lName"
+        placeholder="Last Name"
+        value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
